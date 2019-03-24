56e9d6455b9a0862879796ed9d507cd773a13ec6
Linegod <pasnak@gmail.com>
2019-03-23 22:36:14 -0600

    safe

--- snmp_oid_collect.sh
+++ snmp_oid_collect.sh
@@ -7,15 +7,15 @@ DIR_PATH="/"
 PARSE="table.awk"
 HOSTS="host1 host2"
 OIDS="ifTable"
-for HOST_ID in $HOSTS
+for HOST_ID in ${HOSTS}
 	do
-		mkdir -p $DIR_PATH/$HOST_ID
-		for OID in $OIDS
+		mkdir -p ${DIR_PATH}/${HOST_ID}
+		for OID in ${OIDS}
 			do
-				DATE=`date +%Y-%m-%dT%T%:z`;DPATH=`date +%Y%m%d`;STAMP="$DATE $HOST_ID $OID"
-				snmpbulkwalk -m ALL -v2c -c$COMMUNITY $HOST_ID -OUQs $OID | 
+				DATE=`date +%Y-%m-%dT%T%:z`;DPATH=`date +%Y%m%d`;STAMP="${DATE} ${HOST_ID} ${OID}"
+				snmpbulkwalk -m ALL -v2c -c${COMMUNITY} ${HOST_ID} -OUQs ${OID} | 
 				sed 's/\.0//g'|
 				sed 's/ = /=/g'|
-				xargs echo $DATE $HOST_ID $OID >> $DIR_PATH/$HOST_ID/$OID-$DPATH.log
+				xargs echo ${DATE} ${HOST_ID} ${OI}D >> ${DIR_PATH}/${HOST_ID}/${OID}-${DPATH}.log
 			done
 	done
